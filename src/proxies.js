4ff27dbd952610fdd06bd0e40c23528103faaf42
Mike Chadwick <mike@mikedigitize.com>
2016-08-10 17:01:35 +0100

    First getter and setter for a Proxied object

--- src/proxies.js
+++ src/proxies.js
@@ -1,1 +1,26 @@
-console.log('proxies!!!');
+let myData = {  
+  name: 'Mike',
+  age: 35
+};
+
+let handler = {  
+  get(target, key) {
+    return Reflect.get(target, key);
+  },
+  set(target, key, value) {
+  	if(key === 'age') {
+  		console.warn(`You can't change your age no matter how much you want to sorry ${Reflect.get(target, 'name')}`);
+  		return Reflect.set(target, key, target[key]);
+  	}
+  	else {
+  		return Reflect.set(target, key, value);
+  	}
+  }
+}
+
+myData = new Proxy(myData, handler);
+console.log('name', myData.name);
+myData.name = 'Dave';
+console.log('name', myData.name);
+myData.age = 21;
+console.log('age', myData.age);
